727fea5bbc2c104856e3c5f877851c6c59e62b2a
DIDI-SU <jisoodidi86@gmail.com>
2022-07-28 14:33:43 +0900

    ✨ 메인페이지 버튼 컴포넌트 추가완료

--- src/components/Btn/Btn.js
+++ src/components/Btn/Btn.js
@@ -1,6 +1,12 @@
-const Btn = ({ children }) => {
+const Btn = ({ children, isSelected, id, handleClick }) => {
+  const isSame = isSelected.id === parseInt(id);
+  const result = isSame ? "  bg-mainColor " : " bg-backGround";
   return (
-    <button className=" w-20 h-8 border border-mainColor rounded-3xl">
+    <button
+      id={id}
+      onClick={e => handleClick(e)}
+      className={`w-20 h-8 border border-mainColor rounded-3xl mx-1 ${result}`}
+    >
       {children}
     </button>
   );
--- src/pages/Main/MainContainer/MainContainer.js
+++ src/pages/Main/MainContainer/MainContainer.js
@@ -1,6 +1,28 @@
+import { useState } from "react";
 import Btn from "../../../components/Btn/Btn";
 
+const FILTER = [
+  { id: 1, content: 20000 },
+  { id: 2, content: 30000 },
+  { id: 3, content: 40000 },
+  { id: 4, content: 50000 },
+];
+const FILTER_MAP = new Map(FILTER.map(item => [item.id, item.content]));
+
 const MainContainer = ({ children }) => {
+  const [isSelected, setIsSelected] = useState({ id: 1, content: 20000 });
+
+  const handleClick = e => {
+    const { id } = e.target;
+    if (isSelected.id === parseInt(id)) {
+      return;
+    } else
+      setIsSelected({
+        id: parseInt(id),
+        content: FILTER_MAP.get(parseInt(id)),
+      });
+  };
+
   return (
     <article className=" py-10 border-Black-100">
       <section>
@@ -9,10 +31,22 @@ const MainContainer = ({ children }) => {
             Top lists in your area
           </h1>
         </div>
-        <div className=" py-6">
-          <Btn />
-          <p className=" text-40 py-2 font-semibold">
-            얼마 이하의 와인이 모여있어요{" "}
+        <div className=" py-6 ">
+          <div className=" flex items-center ">
+            {FILTER.map(items => {
+              const { id, content } = items;
+              return (
+                <Btn
+                  id={id}
+                  key={content}
+                  isSelected={isSelected}
+                  handleClick={handleClick}
+                />
+              );
+            })}
+          </div>
+          <p className=" text-40 py-3 font-semibold">
+            {isSelected.content.toLocaleString()}원 이하의 와인이 모여있어요
           </p>
         </div>
       </section>
