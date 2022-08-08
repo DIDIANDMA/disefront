e773e0cde829a38a593aca1157b4a0319e548b16
DIDI-SU <jisoodidi86@gmail.com>
2022-08-08 15:38:13 +0900

    ✨ 로그인 기능 추가

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,3 +1,5 @@
+import KakaoLogin from "react-kakao-login";
+
 const Login = () => {
   return (
     <section className=" flex items-center justify-between    rounded-lg  w-2/4  bg-backGoround  ">
@@ -8,7 +10,21 @@ const Login = () => {
           src="/images/Login/1.jpg"
         />
       </div>
-      <div className=" flex items-center justify-between mx-auto "> </div>
+      <div className=" flex items-center justify-between mx-auto ">
+        <KakaoLogin
+          token={process.env.REACT_APP_KAKAO_KEY}
+          onSuccess={message => console.log(message)}
+          onFail={err => console.log(err)}
+        >
+          <button id="custom-login-btn">
+            <img
+              src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
+              width="222"
+              alt="카카오 로그인 버튼"
+            />
+          </button>
+        </KakaoLogin>
+      </div>
     </section>
   );
 };
