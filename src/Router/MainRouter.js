6a9f60617b1dce510d2b3c6fdb5ff9268aaf8f21
dev-sese <dev.shhong@gmail.com>
2022-08-08 21:13:41 +0900

    prettier plugin 설치

--- src/Router/MainRouter.js
+++ src/Router/MainRouter.js
@@ -1,3 +1,26 @@
-const MainRouter = () => {};
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Main from "../pages/Main/Main";
+import Modal from "../components/Modal/Modal";
+import Login from "../pages/Login/Login";
+import { ModalProvider } from "../Contexts/ModalContext/ModalContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const MainRouter = () => {
+  return (
+    <BrowserRouter>
+      <ModalProvider>
+        <Modal>
+          <Login />
+        </Modal>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+        <Footer />
+      </ModalProvider>
+    </BrowserRouter>
+  );
+};
 
 export default MainRouter;
